8c7c252998e6cc18fccefb0c2d58bba517063973
thornbird <thornbird.blue@gmail.com>
2021-03-17 21:08:51 +0800

    modify code

--- ak_api.py
+++ ak_api.py
@@ -3,5 +3,6 @@ import akshare as ak
 
 stock='sh000001'
 
-data = ak.stock_zh_index_daily(stock)
+#data = ak.stock_zh_index_daily(stock)
+data = ak.fund_em_open_fund_rank()
 print(data)
--- fund_info2jpg.py
+++ fund_info2jpg.py
@@ -4,15 +4,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 fund_num='110003'
-data="单位净值走势"
+#data="单位净值走势"
+data="累计收益率走势"
 
 name_df = ak.fund_em_fund_name()
 fund_name=fund_num+name_df.loc[name_df['基金代码']==fund_num]['基金简称'].item()
 
-fund_em_info_df = ak.fund_em_open_fund_info(fund=fund_num, indicator=data)
-df = fund_em_info_df.astype({'x':'datetime64','y':'float','equityReturn':'float'})
+df = ak.fund_em_open_fund_info(fund=fund_num, indicator=data)
+#df['日增长率']=df['日增长率'].astype('float')
 print(df)
 
-df.plot(x='x',subplots=True,title=fund_name)
+df.plot(x='净值日期',figsize=(20,10),grid=True,title=fund_name).get_figure().savefig('fund.jpg')
+#df.plot(x='净值日期',figsize=(20,10),grid=True,title=fund_name,secondary_y=['日增长率']).get_figure().savefig('fund.jpg')
 
-plt.savefig('fund.jpg')
